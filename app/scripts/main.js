20043f000829b406d70827f952bb9344ff543c7a
Greeley Dawson <hgd4th@gmail.com>
2014-06-19 16:48:48 -0400

    Pull github user info

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('The Iron Yard Rocks');
+var template = _.template( $('#headerData').html() );
+var pullData = function (data) {
+  $('#headerDataWrap').html( template(data)) ;
+};
+
+var cID = clientID || '';
+var cSECRET = clientSecret || '';
+
+$.getJSON('https://api.github.com/users/4sight', {client_id: cID, client_secret: cSECRET}, function (data) {
+	pullData(data);
+});
